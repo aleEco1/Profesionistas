1d3c568678be5e0a8ac4ef58a7f80b060d727e5f
aleEco1 <alejandro.martinez1unam@gmail.com>
2025-04-09 22:33:28 -0600

    cambios

--- app.py
+++ app.py
@@ -9,25 +9,35 @@ df = pd.read_csv("https://raw.githubusercontent.com/aleEco1/aplicacion/main/dato
 app.layout = html.Div([
     html.H1('Sueldo promedio por profesionista en México (con licenciatura entre 20 y 30 años)', style={'textAlign': 'center', 'color': "blue", 'font-size': 30}),
     html.P('Proporción por número de profesionistas en cada estado', style={'textAlign':'center', 'color': 'black'}),
+
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': c, 'value': c} for c in df['Descripción_Campo_amplio'].unique()],
         value='Administración y negocios', 
         style = {"width":"500px"}
-    ),   
+    ),  
+
+    dcc.Dropdown(
+        id='year-dropdown',
+        options=[{'label': str(year), 'value': year} for year in sorted(df['Año'].unique())],
+        value=df['Año'].min(),  # Valor inicial como el primer año
+        style = {"width":"500px"}
+    ),  
+
     dcc.Graph(id='graph'), 
     dcc.Graph(id='bar-graph'), 
     html.Label("Con datos de ENOE(INEGI). Registros con sueldos declarados", style = {"color": "black"})
 ])
 
-@app.callback([
-    Output('graph', 'figure'),
-    Output('bar-graph', 'figure')],
-    Input('dropdown', 'value')
+@app.callback(
+    [Output('scatter-graph', 'figure'),
+     Output('bar-graph', 'figure')],
+    [Input('dropdown', 'value'),
+     Input('year-dropdown', 'value')]
 )
-def update_graph(selected_country):
+def update_graph(selected_career, selected_year):
     # Filtro general
-    filtered_df = df[df['Descripción_Campo_amplio'] == selected_country]
+    filtered_df = df[df['Descripción_Campo_amplio'] == selected_career]
     
     # Figura principal
     ffig = px.scatter(
@@ -47,7 +57,7 @@ def update_graph(selected_country):
     # Datos de Guanajuato para el mismo campo
     df_gto = df[
         (df["DESCRIP"] == "Guanajuato") &
-        (df["Descripción_Campo_amplio"] == selected_country)
+        (df["Descripción_Campo_amplio"] == selected_career)
     ]
 
     # Añadir traza secundaria (puntos + flecha)
@@ -62,23 +72,24 @@ def update_graph(selected_country):
         ).data[0]
     )
 
+    bar_data = filtered_df[filtered_df['Año'] == selected_year]
     bar_fig = px.bar(
-    filtered_df,
-    x="Descripción_Campo_amplio",  # Profesiones
-    y="Sueldo promedio por profesionista",  # Sueldo promedio por profesionista
-    color="Descripción_Campo_amplio",  # Colores por profesión
-    title=f"Profesiones y Sueldo Promedio en {selected_year}",
-    labels={"Descripción_Campo_amplio": "Profesión", "Sueldo promedio por profesionista": "Sueldo Promedio"},
-    template="simple_white",
-    height=500,
-    width=900
+        bar_data,
+        x="DESCRIP",  # Estados
+        y="Sueldo promedio por profesionista",  # Sueldo promedio
+        color="DESCRIP",  # Colores por estado
+        title=f"Sueldo Promedio por Estado en {selected_year}",
+        labels={"DESCRIP": "Estado", "Sueldo promedio por profesionista": "Sueldo Promedio"},
+        template="simple_white",
+        height=500,
+        width=900
     )
     bar_fig.update_xaxes(tickangle=45)
 
     # Añadir flechas (anotaciones)
 
     
-    return ffig
+    return ffig,bar_fig
 
 
 if __name__ == '__main__':
